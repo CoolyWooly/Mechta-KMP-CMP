428b3bf63603881e6ece7e95a853827cad9357aa
Yerlan <nurakhmetov.erlan@gmail.com>
2025-10-27 20:23:19 +0500

    db

--- core-data/src/commonMain/kotlin/kz/mechta/core_data/data/mapper/CityMapper.kt
+++ core-data/src/commonMain/kotlin/kz/mechta/core_data/data/mapper/CityMapper.kt
@@ -1,5 +1,6 @@
 package kz.mechta.core_data.data.mapper
 
+import kz.mechta.core_data.data.model.CoordinatesDto
 import kz.mechta.core_data.data.model.city.CityDto
 import kz.mechta.core_data.domain.model.CityModel
 
@@ -14,6 +15,20 @@ internal object CityMapper {
             latitude = this.coordinates?.latitude,
             longitude = this.coordinates?.longitude,
             translit = this.translit.orEmpty()
+        )
+    }
+
+    fun CityModel.toDto() : CityDto {
+        return CityDto(
+            code = this.code,
+            kaspiCode = this.kaspiCode,
+            name = this.name,
+            phones = this.phones,
+            coordinates = CoordinatesDto(
+                latitude = this.latitude,
+                longitude = this.longitude
+            ),
+            translit = this.translit
         )
     }
 }
